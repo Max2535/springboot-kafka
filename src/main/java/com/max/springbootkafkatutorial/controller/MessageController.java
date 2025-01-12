3f6afb80edb311994c7f0fee6bbf99037341db0b
Max <kalmro4@gmail.com>
2025-01-12 16:22:41 +0700

    Spring Boot + Kafka Producer and Consumer for String Message

---
+++ src/main/java/com/max/springbootkafkatutorial/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.max.springbootkafkatutorial.controller;
+
+import com.max.springbootkafkatutorial.kafka.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+    @Autowired
+    private KafkaProducer KafkaProducer;
+
+    // http://localhost:8080/api/v1/kafka/publish?message=Hello
+    @GetMapping(value = "/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message) {
+        KafkaProducer.sendMessage(message);
+         return ResponseEntity.ok("Message sent to the Kafka Topic successfully");
+    }
+}
---
+++ src/main/java/com/max/springbootkafkatutorial/kafka/KafkaProducer.java
@@ -0,0 +1,24 @@
+package com.max.springbootkafkatutorial.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${topic.name}")
+    private String topicName;
+
+    public void sendMessage(String message) {
+        LOGGER.info(String.format("Message sent %s", message));
+        kafkaTemplate.send(topicName, message);
+    }
+}
